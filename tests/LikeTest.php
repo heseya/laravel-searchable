00103a018c684fe688465a54ebba7e29dcc50790
bvlinsky <bvlinsky@users.noreply.github.com>
2020-08-14 06:23:53 +0000

    Apply fixes from StyleCI

--- tests/LikeTest.php
+++ tests/LikeTest.php
@@ -1,10 +1,10 @@
 <?php
 
+use Tests\Models\User;
 use Heseya\Searchable\Searches\Like;
 use Illuminate\Database\Eloquent\Builder;
-use Tests\Models\User;
 
-test('asserts query create', function() {
+test('asserts query create', function () {
     $query = User::query();
     $search = new Like('key', 'value');
     $query = $search->query($query);
